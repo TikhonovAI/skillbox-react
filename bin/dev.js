88a3c187281c70d4be93e5c96d0d88afe31dab2b
TikhonovAI <Alex29021988@mail.ru>
2020-07-11 12:03:03 +0300

    hmr-error

--- bin/dev.js
+++ bin/dev.js
@@ -1,9 +1,35 @@
 const webpack = require('webpack');
-const webpackConfig = require('../webpack.config');
+const [webpackClientConfig, webpackServerConfig] = require('../webpack.config');
 const nodemon = require('nodemon');
 const path = require('path');
+const webpackDevMiddleare = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const express = require('express');
 
-const compiler = webpack(webpackConfig);
+const hmrServer = express();
+const clientCompiler =webpack(webpackClientConfig);
+
+hmrServer.use(webpackDevMiddleare(clientCompiler, {
+  publicPath: '/static/',
+  serverSideRender: true,
+  noInfo: true,
+  watchOptios: {
+    ignore: /dist/,
+  },
+  writeToDisk: true,
+  stats: 'errors-only'
+}));
+
+hmrServer.use(webpackHotMiddleware(clientCompiler, {
+  path: '/staic/__webpack_hmr'
+}));
+
+hmrServer.listen(3001, () => {
+  console.log('HMR Srver successfully started');
+  
+});
+
+const compiler = webpack(webpackServerConfig);
 compiler.run((err) => {
     if (err) {
         console.log('Compilation failed', err);
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -3,5 +3,5 @@ import * as ReactDOM from "react-dom";
 import { Header } from "../shared/Header";
 
 window.addEventListener('load', () => {
-    ReactDOM.render(<Header />, document.getElementById('reactRoot'));
+    ReactDOM.hydrate(<Header />, document.getElementById('reactRoot'));
 });
